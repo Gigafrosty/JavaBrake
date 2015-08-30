5b32be29cfe75df4e928b45661434749744df6b8
Emmitt R. Kraft <gigafrost@gmail.com>
2015-08-29 20:16:24 -0500

    Initial check-in.

---
+++ src/ekraft/javabrake/JavaBrake.java
@@ -0,0 +1,9 @@
+package ekraft.javabrake;
+
+
+/**
+ * Created by ekraft on 8/29/15.
+ */
+public class JavaBrake {
+
+}
---
+++ src/ekraft/javabrake/db/Chapter.java
@@ -0,0 +1,9 @@
+package ekraft.javabrake.db;
+
+
+/**
+ * Created by ekraft on 8/29/15.
+ */
+public class Chapter {
+
+}
